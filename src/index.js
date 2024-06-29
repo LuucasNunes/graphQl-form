9aa305ad390a8319a64d4bc4cc25710b314d590d
LuucasNunes <llucas-nunes@hotmail.com>
2024-06-29 18:37:32 -0300

    Day 3 | tests GET_CADASTROS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('Finalizada as configurações de ambiente!');
+import { ApolloServer, gql } from 'apollo-server';
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      id: ID!
+      name: String!
+      age: Int!
+      avarage: Float
+      married: Boolean
+      arrayTest: [String!]!
+    }
+  `,
+
+  resolvers: {
+    Query: {
+      id: () => '7dayd8uh91-8',
+      name: () => 'Lucas Antonelo',
+      age: () => 33,
+      avarage: () => 57.65,
+      married: () => true,
+      arrayTest: () => ['Abc', 'def', 'ghi'],
+    },
+  },
+});
+
+server.listen(4007).then(({ url }) => {
+  console.log(`Server listening on url ${url}`);
+});
